1566139cc9cd0382c1179955b5ebbcf2c77b863f
shreyansh <116005536+codingmonk-yt@users.noreply.github.com>
2023-04-09 08:27:39 +0530

    updated BASE URL

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import { enUS, frFR, zhCN, viVN, arSD } from '@mui/material/locale';
 // routes
 import { PATH_DASHBOARD } from "./routes/paths";
 
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://api.chat.codingmonk.in/";
 
 export const S3_BUCKET_NAME = 'codingmonk';
 export const AWS_ACCESS_KEY = 'AKIARPJQ4HSYLBIK2TDE';
--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client"; // Add this
 let socket;
 
 const connectSocket = (user_id) => {
-  socket = io("http://localhost:3001", {
+  socket = io("https://api.chat.codingmonk.in/", {
     query: `user_id=${user_id}`,
   });
 } // Add this -- our server will run on port 4000, so we connect to it from here
